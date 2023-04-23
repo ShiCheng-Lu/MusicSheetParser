51ec06403be86b18180bd61ecb99a487b7fbf398
ShiCheng Lu <shichenglu0708@gmail.com>
2023-04-23 14:42:57 -0400

    refactor editor

--- src/editor/bar_editor.py
+++ src/editor/bar_editor.py
@@ -1,14 +1,78 @@
-from common.music import Bar
-from pygame_gui.elements import UITextEntryLine
+import common.music
+import pygame_gui
+import pygame
+import editor.pygame_utils
+from editor.note_editor import Note
+from common.label import Bbox
 
-duration_selector = UITextEntryLine()
+width = 180
+menu = None
+
+class BarEditorMenu():
+    def __init__(self, manager) -> None:
+        global menu
+        menu = self
+
+        self.panel = pygame_gui.elements.UIPanel(relative_rect=pygame.Rect(900, 0, 180, 860))
+
+        self.duration_selector = pygame_gui.elements.UIButton(
+            relative_rect=pygame.Rect(0, 0, width, 50),
+            container=self.panel,
+            manager=manager,)
+        self.add_note_button = pygame_gui.elements.UIButton(
+            relative_rect=pygame.Rect(0, 0, width, 50),
+            container=self.panel,
+            manager=manager,
+            text="Add Note")
 
 
+class Bar(common.music.Bar):
+    def __init__(self, bar: common.music.Bar, parent_staff):
+        bar.copy(self)
+        self.parent_staff = parent_staff
+        self.notes: list[Note] = [Note(note, self) for note in self.notes]
 
+        self.valid = False
+        self.validate()
 
-def process_event(event):
-    pass
+    def validate(self):
+        duration = 0
+        target_duration = self.parent_staff.parent_music.time_sig_duration
 
-class BarEditor(Bar):
-    def __init__(self):
-        pass
+        group_duration = 0
+        group_x_end = 0
+        for note in sorted(self.notes, key=lambda x: x.x_min):
+            if note.x_min > group_x_end:
+                # new group
+                duration += group_duration
+                group_x_end = note.x_max
+                group_duration = note.duration
+            else:
+                group_duration = max(group_duration, note.duration)
+                group_x_end = max(group_x_end, note.x_max)
+        duration += group_duration
+
+        self.valid = duration % target_duration == 0
+
+    def render(self, screen):
+        for note in self.notes:
+            note.render(screen)
+        
+        # don't outline if its valid
+        if self.valid:
+            return
+
+        thickness = 3
+
+        rect = editor.pygame_utils.to_pygame_rect(self)
+        pygame.draw.rect(screen, (200, 25, 25), rect, thickness)
+    
+    def delete_note(self, note):
+        self.notes.remove(note)
+    
+    def select(self, pos: Bbox):
+        for note in self.notes:
+            if result := note.select(pos):
+                return result
+        if self.intersects(pos):
+            return self
---
+++ src/editor/note_editor.py
@@ -0,0 +1,162 @@
+import pygame
+import common.music
+import pygame_gui
+from common.label import Bbox
+import editor.pygame_utils
+
+pygame.font.init()
+font = pygame.font.Font('freesansbold.ttf', 20)
+
+TONE_MAP = [
+    "A0", "B0", "C1", "D1", "E1", "F1", "G1",
+    "A1", "B1", "C2", "D2", "E2", "F2", "G2",
+    "A2", "B2", "C3", "D3", "E3", "F3", "G3",
+    "A3", "B3", "C4", "D4", "E4", "F4", "G4",
+    "A4", "B4", "C5", "D5", "E5", "F5", "G5",
+    "A5", "B5", "C6", "D6", "E6", "F6", "G6",
+    "A6", "B6", "C7", "D7", "E7", "F7", "G7",
+    "A7", "B7",
+]
+A4_POS = TONE_MAP.index("A4")
+
+MOD_MAP = {"None": None, "Natural": 0, "Sharp": 1, "Flat": -1, "DoubleSharp": 2, "DoubleFlat": -2}
+
+w, h = 1080, 860
+menu_rect = Bbox([900, 0, 1080, 860])
+width = 180
+
+
+class Note(common.music.Note):
+    def __init__(self, note: common.music.Note, parent_bar):
+        note.copy(self)
+        self.parent_bar = parent_bar
+
+        self.render_color = (25, 200, 25)
+
+    def render(self, screen):
+        thickness = 3
+
+        text = font.render(self.pitch_str, True, (25, 25, 255))
+
+        textRect = text.get_rect()
+        textRect.x = self.x_min
+        textRect.bottom = self.y_min
+
+        rect = editor.pygame_utils.to_pygame_rect(self)
+        rect.width += thickness
+        rect.height += thickness
+
+        pygame.draw.rect(screen, self.render_color, rect, thickness)
+        screen.blit(text, textRect)
+
+    def update(self, duration, pitch, modifier):
+        self.duration: float = duration
+        self.pitch: int = pitch
+        self.modifier = modifier
+
+        self.parent_bar.validate()
+        pass
+    
+    def select(self, pos: Bbox):
+        if self.intersects(pos):
+            return self
+
+
+class NoteEditorMenu():
+    def __init__(self, manager):
+        self.panel = pygame_gui.elements.UIPanel(relative_rect=pygame.Rect(900, 0, 180, 860))
+
+        self.pitch_label = pygame_gui.elements.UILabel(
+            relative_rect=pygame.Rect(0, 0, width, 50),
+            container=self.panel,
+            manager=manager,
+            text="Note Pitch:",)
+        self.pitch_selector = pygame_gui.elements.UIDropDownMenu(
+            relative_rect=pygame.Rect(0, 0, width, 50),
+            container=self.panel,
+            manager=manager,
+            anchors={"top_target": self.pitch_label},
+            options_list=common.music.SEMITONE_MAP,
+            starting_option="A4",)
+        
+        self.modifier_label = pygame_gui.elements.UILabel(
+            relative_rect=pygame.Rect(0, 0, width, 50),
+            container=self.panel,
+            manager=manager,
+            anchors={"top_target": self.pitch_selector},
+            text="Note Modifier:",)
+        self.modifier_selector = pygame_gui.elements.UIDropDownMenu(
+            relative_rect=pygame.Rect(0, 0, width, 50),
+            container=self.panel,
+            manager=manager,
+            anchors={"top_target": self.modifier_label},
+            options_list=MOD_MAP,
+            starting_option="None",)
+
+        self.duration_label = pygame_gui.elements.UILabel(
+            relative_rect=pygame.Rect(0, 0, width, 50),
+            container=self.panel,
+            manager=manager,
+            anchors={"top_target": self.modifier_selector},
+            text="Note Duration:",)
+        self.duration_selector = pygame_gui.elements.UIHorizontalSlider(
+            relative_rect=pygame.Rect(0, 0, width, 50),
+            container=self.panel,
+            manager=manager,
+            start_value=0,
+            anchors={"top_target": self.duration_label},
+            value_range=(0, 32),)
+        
+        self.delete_button = pygame_gui.elements.UIButton(
+            relative_rect=pygame.Rect(0, 0, width, 50),
+            container=self.panel,
+            manager=manager,
+            anchors={'top_target': self.duration_selector},
+            text="Delete Note")
+        
+        self.save_button = pygame_gui.elements.UIButton(
+            relative_rect=pygame.Rect(0, -50, width, 50),
+            container=self.panel,
+            manager=manager,
+            anchors={'bottom': 'bottom'},
+            text="Save")
+    
+    def set_selected(self, note: Note):
+        self.selected = note
+        if note:
+            tone = TONE_MAP[note.pitch + A4_POS]
+            # set pitch selector
+            self.pitch_selector.selected_option = tone
+            self.pitch_selector.current_state.selected_option = tone
+            self.pitch_selector.current_state.start()
+
+            # set duration selector
+            self.duration_selector.set_current_value(note.duration * 32)
+            self.update()
+    
+    def update(self):
+        duration_text = common.music.display_duration(self.duration_selector.current_value / 32)
+        self.duration_label.set_text(f"Note Duration: {duration_text}")
+
+    def process_event(self, event):
+        if event.type == pygame_gui.UI_HORIZONTAL_SLIDER_MOVED:
+            if event.ui_element == self.duration_selector:
+                self.update()
+        
+        if event.type == pygame_gui.UI_BUTTON_PRESSED:
+            if event.ui_element == self.delete_button and self.selected:
+                bar = self.selected.parent_bar
+                bar.delete_note(self.selected)
+                bar.validate()
+            
+            if event.ui_element == self.save_button and self.selected:
+                self.selected.update( 
+                    self.duration_selector.current_value / 32,
+                    TONE_MAP.index(self.pitch_selector.selected_option) - A4_POS,
+                    MOD_MAP[self.modifier_selector.selected_option])
+
+            if self.on_update != None:
+                self.on_update()
+
+
+
